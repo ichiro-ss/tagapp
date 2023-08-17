5dc9680883c822b1e8845968e7bd0fdc4624a853
LabLaptop <a19.whet@g.chuo-u.ac.jp>
2023-08-17 22:35:13 +0900

    ログイン画面（外観のみ）

--- front-end/front-app/src/pages/login.tsx
+++ front-end/front-app/src/pages/login.tsx
@@ -7,8 +7,24 @@ import { useRouter } from 'next/router'
 
 export default function login() {
   return (
-    <div>
-      !!! This is login page !!!
-    </div>
+    <>
+      <Header title="login"/>
+      <div className="container-fluid vh-100 d-flex align-items-center justify-content-center">
+        <div className="w-50 ">
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="userIDForm">UserID</span>
+            <input type="text" className="form-control" aria-label="userid" aria-describedby="userIDform"/>
+          </div>
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="pwForm">Password</span>
+            <input type="password" className="form-control"  aria-describedby="pwForm"/>
+          </div>
+          <div className="btn-toolbar d-flex justify-content-center">
+            <button type="button" className="btn btn-primary me-2">Submit</button>
+            <button type="button" className="btn btn-primary">Login</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
