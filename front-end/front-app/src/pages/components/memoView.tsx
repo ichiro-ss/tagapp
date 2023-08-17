ad58ecd0797df5855f64b93da1ff4d2b6d67d8ee
LabLaptop <a19.whet@g.chuo-u.ac.jp>
2023-08-17 15:19:25 +0900

    chenge index.tsx

--- front-end/front-app/src/pages/components/memoView.tsx
+++ front-end/front-app/src/pages/components/memoView.tsx
@@ -1,16 +1,6 @@
 import Head from 'next/head'
 import { useState } from 'react';
-import useSWR from 'swr';
-
-type Memo = {
-  title :string,
-  author:string,
-  body  :string,
-  filepath:string,
-  id: number,
-  date:string,
-  tags:string[]
-};
+import MemoDetailedView from './memoDetailedView';
 
 export default function MemoView(): JSX.Element {
   const sampleMemoAry=[
@@ -47,12 +37,12 @@ export default function MemoView(): JSX.Element {
       {/* 3-column */}
       <div className="container-fluid">
         <div className="row vh-100 overflow-auto">
-          <div className="col-3 h-100 border left-column overflow-auto">
+          <div className="col-2 h-100 border left-column overflow-auto">
           </div>
-          <div className="center-column col-3 h-100 border overflow-auto">
+          <div className="center-column col-5 h-100 border overflow-auto">
             <MemoSelector handler={setReadingMemo} />
           </div>
-          <div className="right-column col-6 h-100 border border-warning overflow-auto">
+          <div className="right-column col-5 h-100 border border-warning overflow-auto">
             <MemoDetailedView memo={sampleMemoAry[readingMemo]}/>
           </div>
         </div>
@@ -72,63 +62,6 @@ function MemoSelector({handler}:any): JSX.Element{
         onClick={()=>{ handler(1) }}>
         Memo2
       </button>
-    </div>
-  );
-}
-
-function MemoDetailedView({memo}: {memo?:Memo}): JSX.Element{
-  if(memo == null){
-    return(
-      <div className='detailedView m-1'>
-      </div>
-    );
-  }
-
-  return(
-    <div className='detailedView m-1'>
-      <div className='title'>
-        <h1> {memo.title} </h1>
-      </div>
-      <div className='author'>
-        <h2> Author : {memo.author} </h2>
-      </div>
-      <PreviewDoc filepath={memo.filepath} />
-      <div className='body'>
-        { memo.body.split("\n").map((e) => <div>{e}</div>) }
-      </div>
-      <br/>
-      <div className='date'>
-        {memo.date}
-      </div>
-      <div className='tags'>
-        { memo.tags.map((tag) => "#"+tag+" ") }
-      </div>
-    </div>
-  );
-}
-
-function PreviewDoc({filepath}:{filepath:string}): JSX.Element{
-  if(filepath===""){ return <></>; }
-
-  const fetcher = (url:string) => fetch(url).then(r => r.blob())
-  const {data, error, isLoading}=useSWR(filepath, fetcher);
-
-  if(!!error) return <div>failed to load</div>;
-  if(isLoading) return <div>loading...</div>;
-
-  if(data==null) return <></>;
-  // if(data.type==="application/pdf"){
-  //   return(
-  //     <div>
-  //       <canvas id='pdf-canvas'>
-  //       </canvas>
-  //     </div>
-  //   );
-  // }
-  return(
-    <div>
-      <iframe id='inlineDoc' title='Inline image' className='w-100' src={URL.createObjectURL(data)}/>
-      {/* PDFはこのままでは見ずらい PDF.js? */}
     </div>
   );
 }
