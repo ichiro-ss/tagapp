57a5d1948fb35e89f8714bae14d60125d6f2a07a
LabLaptop <a19.whet@g.chuo-u.ac.jp>
2023-08-17 19:53:59 +0900

    GET API

--- front-end/front-app/src/pages/index.tsx
+++ front-end/front-app/src/pages/index.tsx
@@ -3,25 +3,50 @@ import { useState } from "react";
 import { Back_Index } from "./constants";
 import { LeftSideComponent } from "./components/leftsideComponent";
 import MemoList from "./components/memoList";
+import { useRouter } from 'next/router'
 
 export default function Home() {
-
   const title = "TestApp"
-  const message = "TagAppの左側の実装画面"
+  const [isLoggedIn , setIsLoggedIn]=useState<boolean|undefined>(undefined);
+  const axios = require('axios').default;
+  const router = useRouter();
 
-return (
-  <div>
-    <Header title={title} />
-    {/* 左側表示 */}
-    <div>
-    <LeftSideComponent />
-    </div>
-    {/* メモ表示 */}
-    <div>
-      <MemoList />
-    </div>
+  axios.get('__/api/login__')
+    .then(
+      function (r:any):void{
+        if(r.status===200){
+          setIsLoggedIn(true);
+        }
+        else router.push("./login");
+      }
+    ).catch( (error:any) =>{ 
+      console.log("error");
+      router.push("./login");
+    }
+  )
   
-  //</div>
-);
+  if(isLoggedIn==undefined){
+    return <>Loading...</>;
+  }
+
+  if(isLoggedIn){
+    return (
+      <div>
+        <Header title={title} />
+        {/* 左側表示 */}
+        <div>
+        <LeftSideComponent />
+        </div>
+        {/* メモ表示 */}
+        <div>
+          <MemoList />
+        </div>
+      
+      //</div>
+    );
+  }
+  else{
+    return <></>;
+  }
 
 }
---
+++ front-end/front-app/src/pages/login.tsx
@@ -0,0 +1,14 @@
+import Header from "./components/header";
+import { useState } from "react";
+import { Back_Index } from "./constants";
+import { LeftSideComponent } from "./components/leftsideComponent";
+import MemoList from "./components/memoList";
+import { useRouter } from 'next/router'
+
+export default function login() {
+  return (
+    <div>
+      !!! This is login page !!!
+    </div>
+  );
+}
