3400fdb81a48246ae29c6aa726e2ed20d75af134
segatu <yutarosegawa1820@gmail.com>
2023-08-18 23:42:21 +0900

    UIの改善08/18

--- front-end/front-app/src/pages/components/left/openModalButton.tsx
+++ front-end/front-app/src/pages/components/left/openModalButton.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import styles from '../../../styles/leftside.module.css';
 
 type OpenModalButtonProps = {
     onOpen: () => void;
 };
 
 const OpenModalButton: React.FC<OpenModalButtonProps> = ({ onOpen }) => (
-  <Button variant="secondary" onClick={onOpen}>
+  <Button className={styles["new-memo-button"]} variant="secondary" onClick={onOpen}>
     新規メモを作成
   </Button>
 );
--- front-end/front-app/src/pages/components/leftsideComponent.tsx
+++ front-end/front-app/src/pages/components/leftsideComponent.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MemoData, memos } from "./memoData";
 import { sortedTags, tagCountMap } from "./left/tagCount";
 import TagToggle from "./left/tagToggle";
@@ -8,37 +8,42 @@ import { handleSearch } from "./left/searchUtils";
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import OpenModalButton from "./left/openModalButton";
 import MemoModal from "./left/memoModal";
+import styles from '../../styles/leftside.module.css'; // スタイルを読み込む
+import UserContainer from "./left/userName";
+
+
 
 
 export const LeftSideComponent = () => {
 
   // メモ検索画面
   const [searchResults, setSearchResults] = useState<MemoData[]>([]);
+  const [selectedTag, setSelectedTag] = useState<string | null>(null);
+  const [searchTerm, setSearchTerm] = useState<string>("");
   const onSearch = (
     searchTerm: string,
     tagOnly: boolean,
     andOperator: boolean
   ) => {
-    handleSearch(searchTerm, tagOnly, andOperator, memos, setSearchResults);
+    const actualSearchTerm = selectedTag !== null ? selectedTag : searchTerm;
+    handleSearch(actualSearchTerm, tagOnly, andOperator, memos, setSearchResults);
   };
-
   // タグ一覧
+  const handleTagClick = (tag: string) => {
+    setSearchTerm(tag); // タグを検索バーに表示
+    setSelectedTag(tag); // タグがクリックされたら検索バーにタグをセットする
+    setSearchResults([]); // タグをクリックしたら検索結果をリセット
+    onSearch(tag, false, true); // 検索バーの内容も更新するために onSearch を呼び出す
+  };
+  // const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const newSearchTerm = e.target.value;
+  //   setSearchTerm(newSearchTerm);
+  //   onSearch(newSearchTerm, false, false); // 検索バーの内容も更新するために onSearch を呼び出す
+  // };
   const [showTags, setShowTags] = useState(false); // 初期値をfalseに設定
   const handleToggleTags = () => {
     setShowTags(!showTags);
   };
-
-  const [showPopup, setShowPopup] = useState(false);
-  const handleOpenPopup = () => {
-    setShowPopup(true);
-  };
-  const handleClosePopup = () => {
-    setShowPopup(false);
-  };
-  // const handleCreateMemo = (memo: MemoData) => {
-  //   // 新しいメモの作成処理を実装
-  //   console.log("新規メモを作成:", memo);
-  // };
 
   // 新規メモを作成するポップアップ画面
   const [showModal, setShowModal] = useState(false);
@@ -53,11 +58,17 @@ export const LeftSideComponent = () => {
     console.log('新しいメモを作成:', memo);
   };
 
+  //ユーザー名とアイコンの表示
+  const userId = 'user123';
+
   return (
     <div>
       {/* 検索バーの配置 */}
-      <SearchBar onSearch={onSearch} /> 
-      {searchResults.length > 0 && (
+      <SearchBar 
+        onSearch={onSearch}
+        initialSearchTerm={selectedTag !== null ? selectedTag : searchTerm}
+        />
+      {searchResults.length >= 0 && (
         <div>
           <h4>検索結果</h4>
           <ul>
@@ -72,29 +83,39 @@ export const LeftSideComponent = () => {
           </ul>
         </div>
       )}
+      
 
       {/* タグ一覧を表示 */}
-      <div>
+      <div className={styles["tag-list-container"]}>
         <h4><TagToggle onToggle={handleToggleTags} showTags={showTags} /></h4>
         {showTags && (
-        <div>
-          <ul>
-            {sortedTags.map((tag, index) => (
-              <li key={index}>
-                <p>{tag}({tagCountMap[tag]})</p>
-              </li>
-            ))}
-          </ul>
-        </div>
+          <div className={styles["tag-list"]}>
+            <ul>
+              {sortedTags.map((tag, index) => (
+                <li key={index}>
+                  <button
+                    className={styles["tag-button"] + (selectedTag === tag ? ` ${styles.selectedTag}` : "")}
+                    onClick={() => handleTagClick(tag)}
+                  >
+                    {tag}({tagCountMap[tag]})
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
         )}
       </div>
+
+      {/* ユーザー名とアイコンの表示 */}
+      <div>
+        <UserContainer />
+      </div>
+
       
       {/* メモ作成ボタン and メモ作成モーダル */}
       <OpenModalButton onOpen={handleOpenModal} />
       <MemoModal show={showModal} onClose={handleCloseModal} onCreate={handleCreateMemo} />
-    
-      
-    
+
     </div>
 
   );
