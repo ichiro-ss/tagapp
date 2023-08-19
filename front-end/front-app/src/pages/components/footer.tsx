534e4a5f51b2d3712cc2a4b75198de02b6ca380b
segatu <yutarosegawa1820@gmail.com>
2023-08-19 16:35:40 +0900

    ページ領域で不具合あり

---
+++ front-end/front-app/src/pages/components/footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from '../../styles/index.module.css';
+
+function Footer() {
+    return (
+      <footer className={styles["footer"]}>
+        <p>&copy; 2023 Your Company Name</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
--- front-end/front-app/src/pages/index.tsx
+++ front-end/front-app/src/pages/index.tsx
@@ -1,32 +1,38 @@
 import Header from "./components/header";
+import Footer from "./components/footer";
 import { useState } from "react";
 import { Back_Index } from "./constants";
 import { LeftSideComponent } from "./components/leftsideComponent";
 import MemoList from "./components/memoList";
 import MemoDetailedView from "./components/memoDetailedView";
 import { MemoData } from "./components/memoData";
+// import '../styles/index.module.css';
+
 
 export default function Home() {
 
-  const title = "TestApp"
+  const title = "TagApp"
   const [selectedMemo, setSelectedMemo] = useState<MemoData | undefined>(undefined);
 
   return (
-    <div>
+    <div className="page-container">
+        
+        <div className="container-fluid content">
         <Header title={title} />
-        <div className="container-fluid">
-          <div className="row vh-100 overflow-auto">
-            <div className="col-2 h-100 border left-column overflow-auto">
+          <div className="row">
+            <div className="left-column col-md-auto border overflow-auto">
               <LeftSideComponent />
             </div>
-            <div className="center-column col-5 h-100 border overflow-auto">
+            <div className="center-column col border overflow-auto">
               <MemoList setSelectedMemo={setSelectedMemo}/>
             </div>
-            <div className="right-column col-5 h-100 border overflow-auto">
+            <div className="right-column col border overflow-auto">
               <MemoDetailedView memo={selectedMemo}/>
             </div>
           </div>
+          <Footer />
         </div>
+        
     </div>
   );
 }
