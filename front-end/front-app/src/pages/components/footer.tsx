4dad29752fdd25a4ed048f904de0a3e21ce582fd
segatu <yutarosegawa1820@gmail.com>
2023-08-20 00:52:16 +0900

    footerにユーザーネームを追加

--- front-end/front-app/src/pages/components/footer.tsx
+++ front-end/front-app/src/pages/components/footer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from '../../styles/index.module.css';
+import UserContainer from "./left/userName";
 
 interface FooterProps {
   className?: string; // className プロパティを追加
@@ -8,6 +9,7 @@ interface FooterProps {
 const Footer: React.FC<FooterProps> = (props) => {
     return (
       <div>
+          <UserContainer />
       <footer className={`${styles["footer"]} ${props.className || ''} `}>
         <p>&copy; 2023 Your Company Name</p>
       </footer>
